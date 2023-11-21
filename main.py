7d98d0dae29c651a476fa7c7be458f15ce07e901
Alexander peczon <97857268+BakedSoups@users.noreply.github.com>
2023-11-20 16:23:13 -0800

    created basic functionality
    hard prompt engineered this ngl
    - basic folder reader
    - basic buttons

---
+++ main.py
@@ -0,0 +1,54 @@
+import os
+import tkinter as tk
+from tkinter import ttk, filedialog
+from PIL import Image, ImageTk
+import pygame
+
+class SoundboardApp:
+    def __init__(self, master):
+        self.master = master
+        self.master.geometry("250x275")
+        self.master.title("Soundboard")
+
+        self.queue = []
+
+        # Select folder button
+        ttk.Button(self.master, text="Select Folder", command=self.select_folder).pack(pady=10)
+
+        # Create buttons for each MP3 file in the folder
+        for filename in self.get_mp3_files():
+            ttk.Button(self.master, text=filename, command=lambda f=filename: self.play(f)).pack(pady=5)
+
+    def play(self, filename):
+        full_path = os.path.join(self.selected_folder, filename)
+        pygame.mixer.init()
+        pygame.mixer.music.load(full_path)
+        pygame.mixer.music.play()
+
+    def select_folder(self):
+        self.selected_folder = filedialog.askdirectory()
+        self.update_buttons()
+
+    def get_mp3_files(self):
+        if hasattr(self, 'selected_folder') and self.selected_folder:
+            mp3_files = [f for f in os.listdir(self.selected_folder) if f.endswith(".mp3")]
+            return mp3_files
+        else:
+            return []
+
+    def update_buttons(self):
+        # Clear existing buttons
+        for widget in self.master.winfo_children():
+            widget.destroy()
+
+        # Select folder button
+        ttk.Button(self.master, text="Select Folder", command=self.select_folder).pack(pady=10)
+
+        # Create buttons for each MP3 file in the folder
+        for filename in self.get_mp3_files():
+            ttk.Button(self.master, text=filename, command=lambda f=filename: self.play(f)).pack(pady=5)
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = SoundboardApp(root)
+    root.mainloop()
---
+++ readme.rd
@@ -0,0 +1,9 @@
+## goofy sound board
+ funny sound board project
+
+## To run on you system
+1. Clone the repo
+2. pip install pyautogui tkinter pillow
+3. python main.py
+
+#Todo: 
